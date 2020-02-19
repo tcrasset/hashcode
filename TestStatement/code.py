880c34542957186bb9d33b19e4bd5bf4b0820c9f
Cyril Geortay <c.geortay@student.uliege.be>
2020-02-19 16:02:55 +0100

    Made code PEP8, test commit from Pycharm

--- TestStatement/code.py
+++ TestStatement/code.py
@@ -2,46 +2,49 @@ import numpy as np
 import pandas as pd
 import os
 
-def read_file(filename : str):
-	with open(filename) as file: 
-		data = file.readlines() 
 
-		max_slices = data[0].replace('\n','').split(' ')[0]
-		nb_diff_pizzas = data[0].replace('\n','').split(' ')[1]
+def read_file(filename: str):
+    with open(filename) as file:
+        data = file.readlines()
 
-		pizza_slices = data[1].replace('\n','').split(' ')
+        max_slices = data[0].replace('\n', '').split(' ')[0]
+        nb_diff_pizzas = data[0].replace('\n', '').split(' ')[1]
 
-		pizza_slices_per_type = []
-		for nb_slices in pizza_slices:
-			pizza_slices_per_type.append(nb_slices)
+        pizza_slices = data[1].replace('\n', '').split(' ')
 
-		print(max_slices)
-		print(nb_diff_pizzas)
-		print(pizza_slices_per_type)
+        pizza_slices_per_type = []
+        for nb_slices in pizza_slices:
+            pizza_slices_per_type.append(nb_slices)
 
-	return max_slices, nb_diff_pizzas, pizza_slices_per_type
+        print(max_slices)
+        print(nb_diff_pizzas)
+        print(pizza_slices_per_type)
 
-def solve(max_slices : int, nb_diff_pizzas : int, pizza_slices_per_type : list):
-	pass
-
-def create_submission(nb_pizzas : int, pizza_nb_list : list, directory : str, output_file : str) -> None :
-	filepath = os.path.join(directory, output_file)
-	with open(filepath, 'w') as file:
-		file.write("{}\n".format(nb_pizzas))
-		for pizza_nb in pizza_nb_list:
-			file.write("{} ".format(pizza_nb))	
-		file.write("\n")
-	pass
+    return max_slices, nb_diff_pizzas, pizza_slices_per_type
 
 
-if __name__=='__main__':
-	directory = "/home/tom/Desktop/hashcode/TestStatement"
+def solve(max_slices: int, nb_diff_pizzas: int, pizza_slices_per_type: list):
+    pass
 
-	filename="a_example.in"
 
-	max_slices, nb_diff_pizzas, pizza_slices_per_type = read_file(os.path.join(directory, filename))
+def create_submission(nb_pizzas: int, pizza_nb_list: list, directory: str, output_file: str) -> None:
+    filepath = os.path.join(directory, output_file)
+    with open(filepath, 'w') as file:
+        file.write("{}\n".format(nb_pizzas))
+        for pizza_nb in pizza_nb_list:
+            file.write("{} ".format(pizza_nb))
+        file.write("\n")
+    pass
 
-	solve(max_slices, nb_diff_pizzas, pizza_slices_per_type)
-	output_file = "submit_1.txt"
 
-	create_submission(3, [0, 2, 3], directory = directory, output_file = output_file)
+if __name__ == '__main__':
+    directory = "/home/tom/Desktop/hashcode/TestStatement"
+
+    filename = "a_example.in"
+
+    max_slices, nb_diff_pizzas, pizza_slices_per_type = read_file(os.path.join(directory, filename))
+
+    solve(max_slices, nb_diff_pizzas, pizza_slices_per_type)
+    output_file = "submit_1.txt"
+
+    create_submission(3, [0, 2, 3], directory=directory, output_file=output_file)
