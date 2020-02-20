e8067e6411e26f82501e10b14d7c9d69e60eebf0
tcrasset <tomcrasset@gmail.com>
2020-02-20 21:08:28 +0100

    Fix index out of range in read_input()

--- Hackthon/input.py
+++ Hackthon/input.py
@@ -20,23 +20,27 @@ def read_file(filename: str):
 #                 print(line_1)
                 nb_books = int(line_1[0])
                 nb_libraries = int(line_1[1])
-                nb_days = (line_1[2])
+                nb_days = int(line_1[2])
 
 #                 print(nb_books)
 #                 print(nb_libraries)
 #                 print(nb_days)
             elif(idx == 1):
                 book_scores_with_id = line.replace('\n', '').split(' ')
+                book_scores_with_id = list(map(int,book_scores_with_id))
+
             else:
 
                 if(idx % 2 == 0):
-
+                    
                     #### Per library section
                     line_1 = line.replace('\n', '').split(' ')
-#                     print(line_1)
-                    nb_books_in_library_with_id.append(line_1[0])
-                    signup_days_for_library_with_id.append(line_1[1])
-                    shipping_speed_for_library_with_id.append(line_1[2])
+                    if(line_1[0] == ''):
+                        break
+
+                    nb_books_in_library_with_id.append(int(line_1[0]))
+                    signup_days_for_library_with_id.append(int(line_1[1]))
+                    shipping_speed_for_library_with_id.append(int(line_1[2]))
                     
 #                     print(nb_books_in_library_with_id)
 #                     print(signup_days_for_library_with_id)
@@ -44,6 +48,10 @@ def read_file(filename: str):
                 if(idx % 2 == 1):
 
                     book_ids = line.replace('\n', '').split(' ')
+                    if(book_ids[0] == ''):
+                        break
+
+                    book_ids = list(map(int,book_ids))
                     books_in_library_with_id.append(book_ids)
 #                     print("book ids : {}".format(books_in_library_with_id))
 
@@ -69,7 +77,7 @@ if __name__ == '__main__':
 	signup_days_for_library_with_id, \
 	shipping_speed_for_library_with_id, \
 	book_scores_with_id ,\
-	books_in_library_with_id  = read_file("data/a_example.txt")
+	books_in_library_with_id  = read_file("data/b_read_on.txt")
 
 	# print("nb_books : {}".format(nb_books))
 	# print("nb_libraries : {}".format(nb_libraries))
